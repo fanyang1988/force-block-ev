cde33074de2b19ba0a14159a57d4c7d66a52e479
fanyang <fanyang.coder@gmail.com>
2019-04-24 19:14:43 +0800

    add a p2p peers manager

---
+++ blockev/p2p.go
@@ -0,0 +1,165 @@
+package blockev
+
+import (
+	"encoding/hex"
+	"fmt"
+	"runtime/debug"
+	"sync"
+
+	eos "github.com/eosforce/goforceio"
+	"github.com/eosforce/goforceio/p2p"
+	"go.uber.org/zap"
+)
+
+var logger = zap.NewNop()
+
+func EnableLogging() {
+	logger = eos.NewLogger(false)
+}
+
+type Envelope struct {
+	Peer    string     `json:"peer"`
+	Packet  eos.Packet `json:"packet"`
+	IsClose bool
+}
+
+type Handler interface {
+	Handle(envelope *Envelope)
+}
+
+type HandlerFunc func(envelope *Envelope)
+
+func (f HandlerFunc) Handle(envelope *Envelope) {
+	f(envelope)
+}
+
+type HandlerLogger struct {
+}
+
+func (f HandlerLogger) Handle(envelope *Envelope) {
+	logger.Sugar().Infof("handler ev from %s by %s",
+		envelope.Peer, envelope.Packet.P2PMessage.String())
+}
+
+// P2PPeers a manager for peers to diff p2p node
+type P2PPeers struct {
+	name     string
+	clients  []*p2p.Client
+	handlers []Handler
+	msgChan  chan Envelope
+	wg       sync.WaitGroup
+	chanWg   sync.WaitGroup
+}
+
+// NewP2PPeers new p2p peers from cfg
+func NewP2PPeers(name string, chainID string, peers []string) *P2PPeers {
+	p := &P2PPeers{
+		name:     name,
+		clients:  make([]*p2p.Client, 0, len(peers)),
+		handlers: make([]Handler, 0, 8),
+		msgChan:  make(chan Envelope, 64),
+	}
+
+	cID, err := hex.DecodeString(chainID)
+	if err != nil {
+		logger.Error("decode chain id err", zap.Error(err))
+		panic(err)
+	}
+
+	for idx, peer := range peers {
+		logger.Debug("new peer client", zap.Int("idx", idx), zap.String("peer", peer))
+		client := p2p.NewClient(
+			p2p.NewOutgoingPeer(peer, fmt.Sprintf("%s-%02d", name, idx), &p2p.HandshakeInfo{
+				ChainID:      cID,
+				HeadBlockNum: 1, // TODO from a block to sync
+			}),
+			true,
+		)
+		client.RegisterHandler(p)
+		p.clients = append(p.clients, client)
+	}
+
+	return p
+}
+
+// RegisterHandler register handler to p2ppeers
+func (p *P2PPeers) RegisterHandler(handler Handler) {
+	p.handlers = append(p.handlers, handler)
+}
+
+func (p *P2PPeers) Start() {
+	p.chanWg.Add(1)
+	go func() {
+		defer p.chanWg.Done()
+		for {
+			isStop := p.Loop()
+			if isStop {
+				logger.Info("p2p peers stop")
+				return
+			}
+		}
+	}()
+
+	for idx, client := range p.clients {
+		p.wg.Add(1)
+		go func(i int, cli *p2p.Client) {
+			defer p.wg.Done()
+			// TODO close
+			err := cli.Start()
+			if err != nil {
+				logger.Error("client err", zap.Int("client", i), zap.Error(err))
+			}
+		}(idx, client)
+	}
+}
+
+func (p *P2PPeers) Close() {
+	logger.Warn("start close")
+	for idx, client := range p.clients {
+		go func(i int, cli *p2p.Client) {
+			cli.CloseConnection()
+			logger.Info("client close", zap.Int("client", i))
+		}(idx, client)
+	}
+	p.wg.Wait()
+	p.msgChan <- Envelope{
+		IsClose: true,
+	}
+	close(p.msgChan)
+	p.chanWg.Wait()
+}
+
+func (p *P2PPeers) Loop() bool {
+	ev, ok := <-p.msgChan
+	if ev.IsClose {
+		return true
+	}
+
+	if !ok {
+		logger.Warn("p2p peers msg chan closed")
+		return true
+	}
+
+	for _, h := range p.handlers {
+		func(hh Handler) {
+			defer func() {
+				if err := recover(); err != nil {
+					logger.Error("handler process ev panic",
+						zap.String("err", fmt.Sprintf("err:%s", err)),
+						zap.String("stack", string(debug.Stack())))
+				}
+			}()
+			hh.Handle(&ev)
+		}(h)
+	}
+
+	return false
+}
+
+// Handle handler for p2p clients
+func (p *P2PPeers) Handle(envelope *p2p.Envelope) {
+	p.msgChan <- Envelope{
+		Peer:   envelope.Sender.Address,
+		Packet: *envelope.Packet,
+	}
+}
--- example/p2p-client/main.go
+++ example/p2p-client/main.go
@@ -1,52 +1,54 @@
 package main
 
 import (
-	"encoding/hex"
 	"flag"
 	"fmt"
-	"log"
-	"time"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/eosforce/goforceio/p2p"
+	"github.com/fanyang1988/force-block-ev/blockev"
 )
 
 var chainID = flag.String("chain-id", "bd61ae3a031e8ef2f97ee3b0e62776d6d30d4833c8f7c1645c657b149151004b", "net chainID to connect to")
 var showLog = flag.Bool("v", false, "show detail log")
+
+// Wait wait for term signal, then stop the server
+func Wait() {
+	stopSignalChan := make(chan os.Signal, 1)
+	signal.Notify(stopSignalChan,
+		syscall.SIGINT,
+		syscall.SIGKILL,
+		syscall.SIGQUIT,
+		syscall.SIGUSR1)
+	<-stopSignalChan
+}
 
 func main() {
 	flag.Parse()
 
 	if *showLog {
 		p2p.EnableP2PLogging()
+		blockev.EnableLogging()
+
 	}
 	defer p2p.SyncLogger()
-
-	cID, err := hex.DecodeString(*chainID)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	// from 9001 - 9020
 	const maxNumListen int = 20
 
+	peers := make([]string, 0, maxNumListen+1)
+
 	for i := 0; i < maxNumListen; i++ {
-		go func(idx int) {
-			peer := fmt.Sprintf("127.0.0.1:%d", 9001+idx)
-			fmt.Println("P2P Client ", peer, " With Chain ID :", *chainID)
-			client := p2p.NewClient(
-				p2p.NewOutgoingPeer(peer, fmt.Sprintf("eos-proxy-%02d", idx), &p2p.HandshakeInfo{
-					ChainID:      cID,
-					HeadBlockNum: 1,
-				}),
-				true,
-			)
-
-			client.RegisterHandler(p2p.StringLoggerHandler)
-			client.Start()
-		}(i)
+		peers = append(peers, fmt.Sprintf("127.0.0.1:%d", 9001+i))
 	}
 
-	for {
-		time.Sleep(100 * time.Millisecond)
-	}
+	p2pPeers := blockev.NewP2PPeers("test", *chainID, peers)
+	p2pPeers.RegisterHandler(blockev.HandlerLogger{})
+	p2pPeers.Start()
+
+	Wait()
+
+	p2pPeers.Close()
 }
