8b586564198d1c65e31fc59f43c1143af946c714
fanyang <fanyang.coder@gmail.com>
2019-04-30 17:16:13 +0800

    change handler imp interface to general types

--- blockev/msg.go
+++ blockev/msg.go
@@ -3,22 +3,25 @@ package blockev
 import (
 	eos "github.com/eosforce/goforceio"
 	"github.com/fanyang1988/force-block-ev/log"
+	"github.com/fanyang1988/force-go/types"
 	"go.uber.org/zap"
 )
 
 type P2PMsgHandlerImp interface {
-	OnBlock(peer string, msg *eos.SignedBlock) error
-	OnGoAway(peer string, msg *eos.GoAwayMessage) error
+	OnBlock(peer string, msg *types.BlockGeneralInfo) error
+	OnGoAway(peer string, reason uint8, nodeID types.Checksum256) error
 }
 
 type P2PMsgHandler struct {
-	imp P2PMsgHandlerImp
+	switcher types.SwitcherInterface
+	imp      P2PMsgHandlerImp
 }
 
 // NewP2PMsgHandler Create a handler
 func NewP2PMsgHandler(imp P2PMsgHandlerImp) *P2PMsgHandler {
 	return &P2PMsgHandler{
-		imp: imp,
+		imp:      imp,
+		switcher: types.NewSwitcherInterface(types.FORCEIO),
 	}
 }
 
@@ -46,7 +49,11 @@ func (m P2PMsgHandler) handlerBlock(peer string, msg *eos.SignedBlock) error {
 	//	zap.String("peer", peer),
 	//	zap.Uint32("num", msg.BlockNumber()), zap.String("id", id.String()))
 	if m.imp != nil {
-		return m.imp.OnBlock(peer, msg)
+		b, err := m.switcher.BlockToCommon(msg)
+		if err != nil {
+			return err
+		}
+		return m.imp.OnBlock(peer, b)
 	}
 	return nil
 }
@@ -55,7 +62,7 @@ func (m P2PMsgHandler) handlerGoAway(peer string, msg *eos.GoAwayMessage) error 
 	log.Logger().Debug("go away",
 		zap.String("peer", peer), zap.String("reason", msg.Reason.String()))
 	if m.imp != nil {
-		return m.imp.OnGoAway(peer, msg)
+		return m.imp.OnGoAway(peer, uint8(msg.Reason), types.Checksum256(msg.NodeID))
 	}
 	return nil
 }
